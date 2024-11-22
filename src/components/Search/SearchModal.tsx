bd3c5184bca30afd3ad276e748ea2d8e6c92cd06
kurtheiz <kurtheiz@gmail.com>
2024-11-23 08:20:32 +1100

    deploy

--- src/components/Search/SearchModal.tsx
+++ src/components/Search/SearchModal.tsx
@@ -235,7 +235,7 @@ export function SearchModal({ isOpen, onClose, onSearch, initialSearchHash }: Se
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
-          enterFrom="opacity-0"
+          enterFrom="opacity-0" 
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
