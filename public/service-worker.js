5249916f031253bf436f249bcd879596d4f16843
kurtheiz <kurtheiz@gmail.com>
2024-12-04 09:22:05 +1100

    noi caching

---
+++ public/service-worker.js
@@ -0,0 +1,24 @@
+// Service Worker to prevent caching
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      return response;
+    })
+  );
+});
