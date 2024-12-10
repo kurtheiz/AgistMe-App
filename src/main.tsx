b1df566408c3ed8a821d75bfeb7c6e69809db428
kurtheiz <kurtheiz@gmail.com>
2024-12-11 08:47:28 +1100

    dynamic imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'; // Add React import
 import './index.css'
 
+const App = React.lazy(() => import('./App.tsx')); // Implement dynamic import for App component
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+  <React.Suspense fallback={<div>Loading...</div>}>
+   
+    <App />
+  </React.Suspense>,
 )
