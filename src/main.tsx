6546459912744af448cf9e326d9cd1e97cbc2b94
kurtheiz <kurtheiz@gmail.com>
2024-12-24 14:33:52 +1100

    rum

--- src/main.tsx
+++ src/main.tsx
@@ -4,34 +4,29 @@ import { AwsRum, AwsRumConfig } from 'aws-rum-web';
 import App from './App';
 import { StrictMode } from 'react';
 
-// Only initialize RUM in production
-if (import.meta.env.PROD) {
-  try {
-    const config: AwsRumConfig = {
-      sessionSampleRate: 1,
-      guestRoleArn: "arn:aws:iam::856189703446:role/RUM-Monitor-ap-southeast-2-856189703446-8870057897811-Unauth",
-      identityPoolId: "ap-southeast-2:d4361ad1-a845-40ea-b49a-a387b8d0ec3e",
-      endpoint: "https://dataplane.rum.ap-southeast-2.amazonaws.com",
-      telemetries: ["performance","errors",["http", { recordAllRequests: true }]],
-      allowCookies: true,
-      enableXRay: false,
-      enableRumClient: true
-    };
+try {
+  const config: AwsRumConfig = {
+    sessionSampleRate: 1,
+    identityPoolId: "ap-southeast-2:d4361ad1-a845-40ea-b49a-a387b8d0ec3e",
+    endpoint: "https://dataplane.rum.ap-southeast-2.amazonaws.com",
+    telemetries: ["performance","errors","http"],
+    allowCookies: true,
+    enableXRay: false
+  };
 
-    const APPLICATION_ID: string = '613264ec-c357-429d-9c27-4bd36bb34575';
-    const APPLICATION_VERSION: string = '1.0.0';
-    const APPLICATION_REGION: string = 'ap-southeast-2';
+  const APPLICATION_ID: string = '613264ec-c357-429d-9c27-4bd36bb34575';
+  const APPLICATION_VERSION: string = '1.0.0';
+  const APPLICATION_REGION: string = 'ap-southeast-2';
 
-    // Initialize AWS RUM
-    void new AwsRum(
-      APPLICATION_ID,
-      APPLICATION_VERSION,
-      APPLICATION_REGION,
-      config
-    );
-  } catch (error) {
-    console.warn('Failed to initialize AWS RUM:', error);
-  }
+  // eslint-disable-next-line
+  const awsRum: AwsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
 }
 
 // Get the container element
